48783c1a4665d9160f3414c5da6a992d2a7f5c1c
Berlin Ortega <marlitros200002@gmail.com>
2025-01-01 16:03:06 -0600

    asf

--- src/App.jsx
+++ src/App.jsx
@@ -1,75 +1,10 @@
 import Header from "./components/Header.jsx"
 import Guitar from "./components/Guittar.jsx"
-import { useState, useEffect } from "react"
-import { db } from "./data/db.js"
+import { useCart } from "./hooks/useCart.js"
 
 
 export default function App() {
-const initialCart = () => {
-  const localStorageCart = localStorage.getItem('cart')
-  if(localStorageCart){
-    return JSON.parse(localStorageCart)
-  }else{
-    return []
-  }
-}
-
-  const [data] = useState(db)
-  const [cart, setCart] = useState(initialCart)
-
-  const MAX_ITEMS = 5
-  const MIN_ITEMS = 1
-
-  useEffect(() => {
-    localStorage.setItem('cart', JSON.stringify(cart))
-  },[cart])
-
-  function addToCart(item){
-    const itemExists = cart.findIndex((guitar) => guitar.id === item.id)
-    if(itemExists >= 0){
-      if(cart[itemExists].quantity >= MAX_ITEMS) return
-      const updatedCart = [...cart]
-      updatedCart[itemExists].quantity++
-      setCart(updatedCart)
-    }else{
-      item.quantity = 1
-      setCart([...cart, item])
-    }
-  }
-
-  function removeFromCart(id){
-    setCart((prevCart) => prevCart.filter(guitar => guitar.id != id))
-  }
-
-  function increaseQuantity(id){
-    const updatedCart = cart.map( item => {
-      if(item.id === id && item.quantity < MAX_ITEMS){
-        return{
-          ...item,
-          quantity: item.quantity + 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function decreaseQuantity(id){
-    const updatedCart = cart.map(item => {
-      if(item.id === id && item.quantity > MIN_ITEMS){
-        return{
-          ...item,
-          quantity: item.quantity - 1
-        }
-      }
-      return item
-    })
-    setCart(updatedCart)
-  }
-
-  function clearCart(){
-    setCart([])
-  }
+  const {data, cart, addToCart, removeFromCart, decreaseQuantity, increaseQuantity, clearCart} = useCart()
 
   return (
     <>
@@ -88,14 +23,12 @@ const initialCart = () => {
             <Guitar
               key={guitar.id}
               guitar={guitar}
-              setCart={setCart}
               addToCart={addToCart}
             />
           )
         })}
       </div>
     </main>
-
 
     <footer className="bg-dark mt-5 py-5">
         <div className="container-xl">
